dd4b68c14c17aa0a2ccaf554af81981a6120d63f
SunLee1072 <chlee@knox.edu>
2026-05-18 20:45:53 -0500

    Add private route guard - redirect unauthenticated users to splash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Splash  from './pages/Splash';
 import SignUp  from './pages/SignUp';
 import Home    from './pages/Home';
 import Result  from './pages/Result';
 import Advice  from './pages/Advice';
+
+function PrivateRoute({ element }) {
+  const token = localStorage.getItem('token');
+  return token ? element : <Navigate to="/" replace />;
+}
 
 function App() {
   return (
@@ -11,9 +16,10 @@ function App() {
       <Routes>
         <Route path="/"       element={<Splash />}  />
         <Route path="/signup" element={<SignUp />}  />
-        <Route path="/home"   element={<Home />}    />
-        <Route path="/result" element={<Result />}  />
-        <Route path="/advice" element={<Advice />}  />
+        <Route path="/home"   element={<PrivateRoute element={<Home />} />}    />
+        <Route path="/result" element={<PrivateRoute element={<Result />} />}  />
+        <Route path="/advice" element={<PrivateRoute element={<Advice />} />}  />
+        <Route path="*"       element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
